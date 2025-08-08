ed73519cc2eb6a8dc5517071b22c8391303a605d
mukesh2776 <mukeshsr943@gmail.com>
2025-08-08 21:58:31 +0530

    Update ViewStory.jsx

--- src/ViewStory.jsx
+++ src/ViewStory.jsx
@@ -24,7 +24,7 @@ function ViewStory() {
       {story ? (
         <div className='d-flex justify-content-center align-items-center'>
           <Link to={`https://instagram-backend-8y7y.onrender.com/story/${Number(id)-1}/${tot}`}><i  className="bi bi-arrow-left-circle"></i></Link>
-          < img src={story.imageUrl} alt="Image" className='vh-100' />
+          < img src={`https://instagram-backend-8y7y.onrender.com/assets/${story.imageUrl} `}  alt="Image" className='vh-100' />
           <Link to={`https://instagram-backend-8y7y.onrender.com/story/${Number(id)+1}/${tot}`}><i className="bi bi-arrow-right-circle"></i></Link>
         </div>
       ) : (
